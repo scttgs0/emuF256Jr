772a008fc8628ce78865b29828403398b3a09256
Scott Giese <scttgs0@gmail.com>
2022-08-30 19:30:10 -0500

    VIA Registers

---
+++ MemoryLocations/VIA.cs
@@ -0,0 +1,30 @@
+namespace FoenixCore.MemoryLocations
+{
+    public static partial class MemoryMap
+    {
+        public static class VIA
+        {
+            public const ushort BASE            = 0xDC00;
+
+            public const ushort IRB             = 0xDC00;
+            public const ushort ORB             = 0xDC00;
+            public const ushort IRA             = 0xDC01;
+            public const ushort ORA             = 0xDC01;
+            public const ushort DDRB            = 0xDC02;
+            public const ushort DDRA            = 0xDC03;
+            public const ushort T1C_L           = 0xDC04;
+            public const ushort T1C_H           = 0xDC05;
+            public const ushort T1L_L           = 0xDC06;
+            public const ushort T1L_H           = 0xDC07;
+            public const ushort T2C_L           = 0xDC08;
+            public const ushort T2C_H           = 0xDC09;
+            public const ushort SR              = 0xDC0A;
+            public const ushort ACR             = 0xDC0B;
+            public const ushort PCR             = 0xDC0C;
+            public const ushort IFR             = 0xDC0D;
+            public const ushort IER             = 0xDC0E;
+            public const ushort IRA2            = 0xDC0F;
+            public const ushort ORA2            = 0xDC0F;
+        }
+    }
+}
