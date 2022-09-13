47f29ea9880a908f23c6652be1419a6df92f4507
Scott Giese <scttgs0@gmail.com>
2022-09-13 14:34:28 -0500

    MATH module

---
+++ MemoryLocations/MATH.cs
@@ -0,0 +1,13 @@
+namespace FoenixCore.MemoryLocations
+{
+    public static partial class MemoryMap
+    {
+            public const ushort BASE            = 0xDE00;
+
+            public const ushort UMULT_OPERAND_A = 0xDE00;       // [word]
+            public const ushort UMULT_OPERAND_B = 0xDE02;       // [word]
+
+            public const ushort MULT_OPERAND_A  = 0xDE04;       // [word]
+            public const ushort MULT_OPERAND_B  = 0xDE06;       // [word]
+    }
+}
